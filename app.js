e7a2c6d7a40bef62907f2b14e54a49a391f34f6f
rrkz <r.ravikiranjonnapalli@gmail.com>
2017-05-22 17:07:35 +0530

    Todo initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
+
+var app = express();
+
+app.use(express.static('public'));
+
+//mongoose.connect('mongodb://localhost/mydb');
+
+var personSchema = mongoose.Schema({
+	name 	: String,
+	age		: Number,
+	nationality	: String
+	});
+var Person = mongoose.model("Person", personSchema);
+
+app.get('/', function(req, res){	
+	res.sendFile(__dirname+"/views/"+"index.html");
+});
+
+app.post('/',function(req, res){
+	
+});
+
+
+var server = app.listen(8000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('server running at http://%s:%s',host, port);
+});
